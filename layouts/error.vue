a6e120e97ca2019eae59a8a5359558b3475c47b1
Eduardo San Martin Morote <posva13@gmail.com>
2020-08-05 17:39:20 +0200

    init

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>
+      This must have a very different layout
+    </p>
+    <button>hey</button>
+    <div>
+      <button>ha</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
